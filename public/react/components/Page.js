26c7eed3d925fb3f0e907ab20fa46eb707fb5407
Nay0ne <nayone606@googlemail.com>
2023-05-19 13:22:12 +0100

    added usestate for page view and logic

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useState } from 'react'
+import dateFormat from 'dateformat'
+
 
 export const Page = (props) => {
+  //  usestate to reveal content for the page
+const [showContent, setShowContent] = useState(false);
+
+
 
   return <>
-    <h3>{props.page.title}</h3>
+    <div>
+      {/*  page title*/}
+      <h3>{props.page.title}</h3>
+     {
+      showContent && <div className = 'card'>
+      <div className = 'body'>
+          <p className = 'author'>Author:{}</p>
+          <p className = 'content'>{props.page.content}</p>
+          <p className = 'tags'>Tags:{}</p>
+          <p className = 'date'>Published: {dateFormat(props.page.createdAt, "mmmm dS, yyyy")}</p>
+      </div>
+  </div>
+     }
+     <button onClick={() => setShowContent(!showContent)}></button>
+
+    </div>
   </>
 } 
 	
