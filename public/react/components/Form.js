a4b2e6130140dedbddeffa69ea2da912e2e6e619
Nay0ne <nayone606@googlemail.com>
2023-05-20 16:17:34 +0100

    form input class bits

--- public/react/components/Form.js
+++ public/react/components/Form.js
@@ -13,11 +13,11 @@ export const Form = () => {
         <>
             <form className= "form" onSubmit={handleSubmit} aria-label="form">
                 <h3>Add Page</h3>
-                <input className= "title" type="text" aria-label= "title" value={title} onChange={e => setTitle(e.target.value)}></input>
-                <input className= "content" type="text" aria-label="content" value={content} onChange={e => setContent(e.target.value)}></input>
-                <input className= "name" type="text" aria-label="name" value={name} onChange={e => setName(e.target.value)}></input>
-                <input className= "email"type="text" aria-label="email" value={email} onChange={e => setEmail(e.target.value)}></input>
-                <input className= "tags" type="text" aria-label="tags" value={tags} onChange={e => setTags(e.target.value)}></input>
+                <input className= "title" type="text" aria-label= "title" value={title} onChange={e => setTitle(e.target.value)} placeholder="Add Title" ></input>
+                <input className= "content" type="text" aria-label="content" value={content} onChange={e => setContent(e.target.value)} placeholder="Add your Content" ></input>
+                <input className= "name" type="text" aria-label="name" value={name} onChange={e => setName(e.target.value)} placeholder="Name" ></input>
+                <input className= "email"type="text" aria-label="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email Address" ></input>
+                <input className= "tags" type="text" aria-label="tags" value={tags} onChange={e => setTags(e.target.value)} placeholder="Tags#" ></input>
                 <button type="submit">Create Page</button>
 
             </form>
