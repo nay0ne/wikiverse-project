013c53a88d888ecba0a861ad4850581c90ee0097
Nay0ne <nayone606@googlemail.com>
2023-05-20 10:38:31 +0100

    begin form component

---
+++ public/react/components/Form.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Form = () => {
+
+    return 
+        <>
+            <form>
+                <h3>Add Page</h3>
+                <input className= "title" type="text" value={}></input>
+                <input className= "name" type ='text'></input>
+                <input className= "content" type="text"></input>
+                <input className= "name" type="text"></input>
+                <input className= "email"></input>
+                <label>
+                    <input className= "tags">
+                    </input>
+                </label>
+            </form>
+        </>
+
+}
